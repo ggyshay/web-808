ee7a4aa32935cc419a051bcd7b153aebf19f03fc
yshay <yshay@cuidas.com.br>
2020-08-14 21:28:19 -0300

    add ts ignores

--- src/components/instrument-hack.tsx
+++ src/components/instrument-hack.tsx
@@ -1,27 +1,46 @@
-import * as React from 'react';
+import * as React from "react";
 
 export class InstrumentHack extends React.Component<any> {
-    constructor(props) {
-        super(props);
-    }
+  constructor(props) {
+    super(props);
+  }
 
-    render() {
-        console.log('the selected si ', this.props.selectedInstrument);
-        const childrenWithProps = React.Children.map(this.props.children, (child) => {
-            if (typeof child === 'object') {
-                if (child.key === this.props.selectedInstrument) {
-                    return React.cloneElement(child, { steps: this.props.steps, selected: true });
-                } else {
-                    return React.cloneElement(child, { steps: null, selected: false });
-                }
-            }
-            return child;
-        });
+  render() {
+    console.log("the selected si ", this.props.selectedInstrument);
+    const childrenWithProps = React.Children.map(
+      this.props.children,
+      (child) => {
+        if (
+          typeof child === "object" &&
+          child !== null &&
+          child.hasOwnProperty("key")
+        ) {
+          //@ts-ignore
+          if (child.key === this.props.selectedInstrument) {
+            //@ts-ignore
+            return React.cloneElement(child, {
+              steps: this.props.steps,
+              selected: true,
+            });
+          } else {
+            //@ts-ignore
+            return React.cloneElement(child, { steps: null, selected: false });
+          }
+        }
+        return child;
+      }
+    );
 
-        return (
-            <div style={{ flex: 1, flexDirection: 'row', justifyContent: 'space-between' }}>
-                {childrenWithProps}
-            </div>
-        )
-    }
+    return (
+      <div
+        style={{
+          flex: 1,
+          flexDirection: "row",
+          justifyContent: "space-between",
+        }}
+      >
+        {childrenWithProps}
+      </div>
+    );
+  }
 }
